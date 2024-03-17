e60f7d643c9035992832dd6528ca21bd03e90baf
Dovran123 <davidsefcik4@gmail.com>
2024-03-17 17:15:13 +0100

    uloha cislo 2

--- t4_cv3/my_module.py
+++ t4_cv3/my_module.py
@@ -1,2 +1,10 @@
 def module_foo():
     print("This is a local module")
+is_green = bool;
+is_true = is_green or 0
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pandas import DataFrame
+arr = np.array([1, 2, 3, 4, 5])
+print("Array:", arr)
